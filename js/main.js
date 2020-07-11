0d6688d0237651dbf9cfcc6441c511802e5ce692
yokean1605 <yoke.mahardika@gmail.com>
2020-07-11 22:45:13 +0700

    update

--- js/main.js
+++ js/main.js
@@ -19,8 +19,9 @@
       $(".nav").css({ "box-shadow": "rgba(0, 0, 0, 0.15) 0px 5px 7px -2px" });
       $(".scroll-x").css("max-height", "77vh");
       $(".menu-top").css({ "line-height": "0", "margin-bottom": "0px" });
-      $('.pt4').toggleClass('pt-4 pt-0');
+      $('.pt4').css('padding-top', '0px');
     } else {
+      $('.pt4').css('padding-top', '1.5rem');
       $(".menu-top").css({ "line-height": "1.2", "margin-bottom": "1rem" });
       $(".scroll-x").css({ "max-height": "45vh" });
     }
@@ -38,23 +39,23 @@
   });
 
   $(".scroll-x").scroll(function () {
-    if ($(this).scrollTop() > 100) {
-      $('#back-to-top').fadeIn();
+    if ($(this).scrollTop()) {
+      $('#back-to-top').css('display', 'block');
       $('.footer').css('bottom', '-56px');
     } else {
-      $('#back-to-top').fadeOut();
+      $('#back-to-top').css('display', 'none');
       $('.footer').css('bottom', '0px');
     }
   });
   // scroll body to 0px on click
   $('#back-to-top').click(function () {
-    $('#back-to-top').tooltip('hide');
+    // $('#back-to-top').tooltip('hide');
     $('.footer').css('bottom', '-56px');
-    $(".scroll-x").animate({ scrollTop: 0 }, 300);
+    // $(".scroll-x").animate({ scrollTop: 0 }, 300);
     return false;
   });
 
-  $('#back-to-top').tooltip('show');
+  // $('#back-to-top').tooltip('show');
 
 
 })(jQuery); // End of use strict
