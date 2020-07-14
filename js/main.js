dab28203eeba42aea2051f058eb21cf40c2c90de
yokean1605 <yoke.mahardika@gmail.com>
2020-07-14 14:13:32 +0700

    if scroll up show div

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
-(function ($) {
-  "use strict"; // Start of use strict
-
+$(document).ready(function () {
   $(".caret-tri").on("click", function () {
     $("#i-fas").toggleClass("fa-chevron-down fa-chevron-up");
     $(".card-accordions").toggleClass("border-show");
@@ -14,16 +12,20 @@
 
   $('.scroll-x').on('scroll', function () {
     var y = $(this).scrollTop();
-    if (y) {
+    if (y > 300) {
       $(".menu-top").css({ "line-height": "0", "margin-bottom": "0px" });
       $('.pt4').css('padding-top', '0px');
       $(".h").css("max-height", "0px");
-      $(".scroll-x").css("max-height", "77vh");
+      $(".scroll-x").css("max-height", "83vh");
       $(".nav").css({ "box-shadow": "rgba(0, 0, 0, 0.15) 0px 5px 7px -2px" });
-    } else {
-      $('.pt4').css('padding-top', '1.5rem');
-      $(".menu-top").css({ "line-height": "1.2", "margin-bottom": "1rem" });
-      $(".scroll-x").css({ "max-height": "45vh" });
+    }
+
+    if (y < 100) {
+      setTimeout(function () {
+        $('.pt4').css('padding-top', '1.5rem');
+        $(".menu-top").css({ "line-height": "1.2", "margin-bottom": "1rem" });
+      }, 100);
+      $(".scroll-x").css({ "max-height": "42vh" });
     }
   });
 
@@ -32,18 +34,19 @@
     if (currentScroll == 0) {
       $(".h").css("max-height", "100px");
       $(".nav").css({ "box-shadow": "none" });
-      setTimeout(function () {
-      }, 10);
+
       // // $(".scroll-x").css("max-height", "39vh");
     }
   });
 
   $('.nav-link').on('click', function () {
-    $(".menu-top").css({ "line-height": "0", "margin-bottom": "0px" });
-    $('.pt4').css('padding-top', '0px');
-    $(".h").css("max-height", "0px");
-    $(".scroll-x").css("max-height", "77vh");
-    $(".nav").css({ "box-shadow": "rgba(0, 0, 0, 0.15) 0px 5px 7px -2px" });
+    setTimeout(function () {
+      $(".menu-top").css({ "line-height": "0", "margin-bottom": "0px" });
+      $('.pt4').css('padding-top', '0px');
+      $(".h").css("max-height", "0px");
+      $(".scroll-x").css("max-height", "77vh");
+      $(".nav").css({ "box-shadow": "rgba(0, 0, 0, 0.15) 0px 5px 7px -2px" });
+    }, 10);
   });
 
   $(".scroll-x").scroll(function () {
@@ -55,16 +58,15 @@
       $('.footer').css('bottom', '0px');
     }
   });
+
   // scroll body to 0px on click
   $('#back-to-top').click(function () {
-    // $('#back-to-top').tooltip('hide');
     $('.footer').css('bottom', '-56px');
     $(".scroll-x").animate({ scrollTop: 0 }, 100);
     return false;
   });
+});
 
-
-})(jQuery); // End of use strict
 
 function openNav() {
   document.getElementById("mySidenav").style.width = "100%";
